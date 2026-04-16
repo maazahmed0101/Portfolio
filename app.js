bba7ac31a271aa9da9663a703d44708266eb0ad6
Maaz-Ahmed <wwwmaazahmed1@gmail.com>
2026-04-16 15:54:32 +0500

    update

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-
-
-
-
-
-const words = ['Web Developer','Frontend Developer', 'Freelancer']
-let wordIndex = 0;
-let charIndex = 0;
-let isDeleting = false;
-
+    const words = ['Web Developer','Frontend Developer', 'Freelancer']
+    let wordIndex = 0;
+    let charIndex = 0;
+    let isDeleting = false;
+    let vh = window.innerHeight;
+    document.documentElement.style.setProperty('--fixed-vh',`${vh}px`)
 function type() {
     const currendWord = words[wordIndex]
     const displayElement = document.querySelector('#typing-text')
@@ -25,26 +21,20 @@ function type() {
     } else if (isDeleting && charIndex === 0) {
         isDeleting = false;
         wordIndex = (wordIndex + 1) % words.length;
-
         typeSpeed = 500
     }
     setTimeout(type, typeSpeed)
 }
 type()
-
-// main work
  const btns = document.getElementById('menuBtn');
         btns.addEventListener('click', () => {
             btns.classList.toggle('active');
         });
-    
         (function () {
             emailjs.init("TyHmMX8WhakS1cBwa");
         })();
-
         document.getElementById("contact-form").addEventListener("submit", function (e) {
             e.preventDefault();
-
             emailjs.sendForm("service_2o12paq", "template_8oyy2zb", this)
                 .then(function () {
                     alert("✅ Message Sent Successfully!");
@@ -52,61 +42,39 @@ type()
                     alert("❌ Failed: " + error);
                 });
         });
-
-(function(){
-    emailjs.init("TyHmMX8WhakS1cBwa");
-})();
-
 const form = document.getElementById("contact-form");
 const btn = document.getElementById("sendBtn");
-
 form.addEventListener("submit", function(e){
     e.preventDefault();
-
-    // 🔥 Loading state
     btn.innerText = "Sending...";
     btn.disabled = true;
-
     emailjs.sendForm("service_2o12paq", "template_8oyy2zb", this)
     .then(function(){
-        // ✅ Success popup
         Swal.fire({
             title: "Message Sent!",
             text: "Your message has been delivered successfully 🚀",
             icon: "success",
             confirmButtonColor: "#0ef"
         });
-
         form.reset();
-
         btn.innerText = "Send Message";
         btn.disabled = false;
-
     }, function(error){
-        // ❌ Error popup
         Swal.fire({
             title: "Error!",
             text: "Something went wrong 😢",
             icon: "error"
         });
-
         btn.innerText = "Send Message";
         btn.disabled = false;
     });
 });
-
  btn = document.getElementById('menuBtn');
         btn.addEventListener('click', () => {
             btn.classList.toggle('active');
         });
-    
-        (function () {
-            emailjs.init("TyHmMX8WhakS1cBwa");
-        })();
-
         document.getElementById("contact-form").addEventListener("submit", function (e) {
             e.preventDefault();
-
             emailjs.sendForm("service_2o12paq", "template_8oyy2zb", this)
                 .then(function () {
                     alert("✅ Message Sent Successfully!");
@@ -114,6 +82,3 @@ form.addEventListener("submit", function(e){
                     alert("❌ Failed: " + error);
                 });
         });
-
-
-
