27a0a8b6a56c5a1570fbc0cf7e3cca2daf86091a
Maaz-Ahmed <wwwmaazahmed1@gmail.com>
2026-05-17 10:39:53 +0500

    update

--- Js/app.js
+++ Js/app.js
@@ -39,8 +39,3 @@ button.addEventListener('click', () => {
 
 
 // })
-// const btn = document.getElementById('menuBtn');
-
-// btn.addEventListener('click', () => {
-//     btn.classList.toggle('active');
-// });
